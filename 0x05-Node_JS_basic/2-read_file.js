0b14c8c4c49518a4516e6455dad2a4b3ee0fd886
dina <dinaelhadidy966@gmail.com>
2024-01-29 11:53:24 -0500

    fix lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   try {
@@ -6,21 +6,18 @@ function countStudents(path) {
     data = data.toString().split('\n');
     data = data.filter((item) => item);
     console.log('Number of students:', data.length - 1);
-    obj = {};
-    for (let i = 1; i < data.length; i++) {
+    const obj = {};
+    for (let i = 1; i < data.length; i += 1) {
       const newdata = data[i].split(',');
       const dept = newdata[3];
       const fn = newdata[0];
       if (!(dept in obj)) obj[dept] = [];
       obj[dept].push(fn);
     }
-    //console.log(obj);
     for (const i in obj) {
-      console.log(
-        `Number of students in ${i}: ${obj[i].length}. List: ${obj[i].join(
-          ', '
-        )}`
-      );
+      if (i) {
+        console.log(`Number of students in ${i}: ${obj[i].length}. List: ${obj[i].join(', ')}`);
+      }
     }
   } catch (err) {
     throw new Error('Cannot load the database');
