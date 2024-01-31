1a1acd4542ef0de12cd2a92ecf0e83094a082a48
dina <dinaelhadidy966@gmail.com>
2024-01-31 14:07:28 -0500

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const calculateNumber = require('./0-calcul');
+
+describe("numbers test", function() {
+  it("test both ints", function() {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it("test float and int", function() {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it("test both floats", function() {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it("test string and int", function() {
+    assert.equal(calculateNumber('test', 3), NaN);
+  });
+});
